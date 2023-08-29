b93d48ed76aa6529443ae3c4cb2eb6187605ae83
MrDimar23 <132336056+MrDimar23@users.noreply.github.com>
2023-08-29 21:36:47 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import Slider from './src/components/Slider';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Slider/>
+    </SafeAreaView>
+  )
+}
+
+export default App;
